127c3e343b143429588067661a57738e63df84b1
SolidityCI <corporate@freighttrust.com>
2020-10-25 22:13:11 +0000

    slither output artifacts
    Signed-off-by: SolidityCI <corporate@freighttrust.com>

--- contracts/UniswapOracleFactory.sol
+++ contracts/UniswapOracleFactory.sol
@@ -1,6 +1,6 @@
 // File: @uniswap\v2-core\contracts\interfaces\IUniswapV2Factory.sol
 
-pragma solidity >=0.5.0;
+pragma solidity >=0.6.6;
 
 interface IUniswapV2Factory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
